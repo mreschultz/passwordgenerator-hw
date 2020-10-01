a7a9e51011bbabaeb39854e92c234a4cd19bb8bb
mreschultz <70779836+mreschultz@users.noreply.github.com>
2020-10-01 00:08:49 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// Assignment Code
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+}
+//code referenced from geeksforgeeks.com
+function generatePassword() {
+  var pass = '';
+  var str =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + 'abcdefghijklmnopqrstuvwxyz0123456789@#$';
+
+  for (i = 1; i <= 8; i++) {
+    var char = Math.floor(Math.random() * str.length + 1);
+
+    pass += str.charAt(char);
+  }
+
+  return pass;
+}
+
+function gfg_Run() {
+  el_down.innerHTML = generateP();
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
