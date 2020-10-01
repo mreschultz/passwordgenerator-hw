c5af3cd725b705989a842412787e4d1984784a92
Eric Schultz <70779836+mreschultz@users.noreply.github.com>
2020-10-01 18:31:44 -0400

    Second draft of homework
    Functional second draft to be edited.

---
+++ README.md
@@ -0,0 +1,4 @@
+This is a password generator which can by used to display a random password. 
+The password must contain at least 8 characters up to 128 characters.
+It includes letters, numbers and symbols.
+The code includes variables, prompts and confirms.
--- script.js
+++ script.js
@@ -8,13 +8,87 @@ function writePassword() {
 
   passwordText.value = password;
 }
-//code referenced from geeksforgeeks.com
+
+// Alert to select which criteria to include in the password
+
+// Prompt for the length of the password
+// Confirm THEN I choose a length of at least 8 characters and no more than 128 characters
+
+// Prompt for character types to include in the password
+// Confirm choose lowercase, uppercase, numeric, and/or special characters
+
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+
+// WHEN all prompts are answered
+
+// Used w3schools to help write this section.
+
+var userName = prompt('Would you like a new password?');
+var newpassword = prompt(
+  'I understand my password can be from 8-128 characters long including letters (both lowercase and uppercase), numbers and/or symbols.'
+);
+var acknowledge = confirm('Thank you!');
+
+var userName = prompt('Pick the amount of characters from in your password.');
+var confirmnumbers = confirm('Good choice!');
+
+if (confirmnumbers == null || password == '') {
+  txt = 'User cancelled the prompt.';
+} else {
+  txt =
+    'Try again' +
+    confirmnumbers +
+    'Please enter the amount of characters in your new password';
+}
+
+var userName = prompt('Pick the amount of letters from in your password.');
+var confirmletters = confirm('Good choice!');
+
+if (confirmletters == null || password == '') {
+  txt = 'User cancelled the prompt.';
+} else {
+  txt =
+    'Try again' +
+    confirmnumbers +
+    'Please enter the amount of numbers in your new password';
+}
+
+var userName = prompt('Pick the amount of numbers from in your password.');
+var confirmnumbers = confirm('Good choice!');
+
+if (confirmnumbers == null || password == '') {
+  txt = 'User cancelled the prompt.';
+} else {
+  txt =
+    'Try again' +
+    confirmnumbers +
+    'Please enter the amount of numbers in your new password';
+}
+
+var userName = prompt('Pick the amount of symbols from in your new password.');
+var confirmsymbols = confirm('Good choice!');
+
+if (confirmsymbols == null || password == '') {
+  txt = 'User cancelled the prompt.';
+} else {
+  txt =
+    'Try again' +
+    confirmsymbols +
+    'Enter the amount of symbols in your new password.';
+}
+
+console.log(userName);
+
+// Used geekforgeek to help write this section.
+
 function generatePassword() {
   var pass = '';
   var str =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + 'abcdefghijklmnopqrstuvwxyz0123456789@#$';
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
+    'abcdefghijklmnopqrstuvwxyz0123456789@#$!"#$%&()*+,-.:;<=?@[]^_';
 
-  for (i = 1; i <= 8; i++) {
+  for (i = 1; i <= 128; i++) {
     var char = Math.floor(Math.random() * str.length + 1);
 
     pass += str.charAt(char);
